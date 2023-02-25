88fbaf74d086a374f1d75df9ba00240b09bf8d07
Thiago <thidurante@hotmail.com>
2023-02-25 00:15:03 -0300

    feat: adds exercise 4, working with lists

---
+++ Basic/Exercise4.py
@@ -0,0 +1,34 @@
+"""
+Create a program that manipulates a shopping list
+"""
+
+shopping_list = ['apple', 'banana', 'water']
+
+while True:
+    action = input('What do you want to do? \
+        Insert, delete or list? ').lower()
+    try:
+        if action == 'list':
+            for i, item in enumerate(shopping_list):
+                print(f'{i} {item}')
+        elif action == 'delete':
+            try:
+                index = int(input('Please select index: '))
+                if (shopping_list[index]):
+                    del (shopping_list[index])
+                else:
+                    raise Exception('')
+            except:
+                print('Thats not a valid index')
+        elif action == 'insert':
+            item_to_add = input('What do you want to insert? ').lower()
+            try:
+                if (not item_to_add.isalpha()):
+                    raise Exception('')
+                shopping_list.append(item_to_add)
+            except:
+                print('Item must only contain alphabetical characters')
+        else:
+            raise Exception('')
+    except:
+        print('Please type a valid option')
